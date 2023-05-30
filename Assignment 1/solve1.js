4ef9a5433d807daa3b195b4dcbdd3cd63808d5ec
Debajit <debajit7001@gmail.com>
2023-05-30 23:06:44 +0530

    adding solution

--- Assignment 1/solve1.js
+++ Assignment 1/solve1.js
@@ -54,4 +54,57 @@ function searchInsert(nums, target) {
 //   Q4.
 
 
-// Q5.
+// Q5. Answer
+function mergeSortedArray(nums1, m, nums2, n) {
+    let i = m - 1; // Pointer for nums1
+    let j = n - 1; // Pointer for nums2
+    let k = m + n - 1; // Pointer for the merged array
+    
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+      } else {
+        nums1[k] = nums2[j];
+        j--;
+      }
+      k--;
+    }
+    
+    while (j >= 0) {
+      nums1[k] = nums2[j];
+      j--;
+      k--;
+    }
+  }
+ 
+//   Q 6. Answer
+var containsDuplicate = function(nums) {
+    const map = {}
+    for(const num of nums) {
+        // If we have seen this num before return true
+        if(map[num]) return true
+        map[num] = true
+    }
+    return false
+};
+
+// Q7. Answer
+function moveZeroes(nums) {
+    let nonZeroIndex = 0; // Pointer for non-zero elements
+  
+    // Move non-zero elements to the beginning of the array
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+        nums[nonZeroIndex] = nums[i];
+        nonZeroIndex++;
+      }
+    }
+  
+    // Fill the remaining elements with zeros
+    for (let i = nonZeroIndex; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
+
+//   Q 8. Answer
